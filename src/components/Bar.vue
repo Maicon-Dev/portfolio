8e78095f9c307ba5b369a8d1dd36bbec64432566
Maicon de Morais <maiconmoraisfs@outlook.com>
2021-07-25 20:14:10 -0300

    Toolbar mobile e desktop diferentes(não reponsivo)

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -1,46 +1,24 @@
 <template>
-	<v-app-bar hide-on-scroll="true" app color="rgba(134,37,210, 0.05)" dark>
-		<div class="d-flex align-center">
-			<v-img
-				alt="Home logo"
-				class="shrink mr-2"
-				contain
-				src="https://image.flaticon.com/icons/png/512/1443/1443033.png"
-				transition="scale-transition"
-				width="40"
-			/>
-		</div>
+	<div v-if="displaySize < 1070">
+		<Mobile />
+	</div>
 
-		<v-spacer></v-spacer>
-
-		<router-link to="/">
-			<v-btn text>
-				<span class="mr-2">Home</span>
-			</v-btn>
-		</router-link>
-
-    <router-link to="/about"> 
-      <v-btn text>
-        <span class="mr-2">Sobre mim</span>
-      </v-btn>
-    </router-link>
-
-		<router-link to="/Projetos"> 
-			<v-btn text>
-				<span class="mr-2">Meus projetos</span>
-			</v-btn>
-		</router-link>
-
-		<router-link to="/Curriculo">
-			<v-btn text>
-				<span class="mr-2">Currículo</span>
-			</v-btn>
-		</router-link>
-	</v-app-bar>
+	<div v-else>
+		<Desktop />
+	</div>
 </template>
 
 <script>
+import Mobile from './BarMobile.vue'
+import Desktop from './BarDesktop.vue'
 export default {
-  
+	data: () => ( {
+    displaySize: window.screen.width
+  }),
+
+	components: {
+		Mobile,
+		Desktop
+	},
 }
 </script>
