b3931f66b88bf76b33a5a9eaa43538d29826d82c
Maicon de Morais <maiconmoraisfs@outlook.com>
2021-08-03 20:26:20 -0300

    Requisição github configurada Layout definido

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -3,56 +3,41 @@
 		<Bar />
 		<v-main>
 			<v-container>
-        <h2>Meus projetos</h2>
-        <p>Aqui você poderá acessar todos meus projetos do Github <br>
-        Poderá acessar a demo ou clicar no ícone do github para acessar o repositório</p>
-				<v-sheet id="group" class="mx-auto" elevation="8">
-					<v-slide-group
-						v-model="model"
-						class="pa-4"
-						active-class="success"
-						show-arrows
-						dark
-					>
-						<v-slide-item
-							id="projeto"
-							v-for="n in 4"
-							:key="n"
-							v-slot="{ active, toggle }"
-						>
+				<h2>Meus projetos</h2>
+				<p>
+					Aqui você poderá acessar todos meus projetos do Github <br />
+					Poderá acessar a demo ou clicar no ícone do github para acessar o
+					repositório
+				</p>
 
-            <v-card
-          :color="active ? undefined : 'grey lighten-1'"
-          class="ma-4"
-          height="300"
-          width="300"
-          @click="toggle"
-        >
-          <v-row
-            class="fill-height"
-            align="center"
-            justify="center"
-          >
-            <v-scale-transition>
-              <v-icon
-                v-if="active"
-                color="red"
-                size="48"
-                v-text="'mdi-close-circle-outline'"
-              ></v-icon>
-            </v-scale-transition>
-          </v-row>
-        </v-card>
-							
-						</v-slide-item>
-					</v-slide-group>
-				</v-sheet>
+				<v-slide-group
+					v-model="model"
+					class="pa-4"
+					active-class="success"
+					show-arrows
+					dark
+				>
+					<v-slide-item v-for="n in 4" :key="n" v-slot="{ active, toggle }">
+						<v-card
+							:color="active ? undefined : 'purple'"
+							class="ma-4"
+							height="340"
+							width="300"
+							@click="toggle"
+						>
+							<v-row class="fill-height" align="center" justify="center">
+								Conteudo
+							</v-row>
+						</v-card>
+					</v-slide-item>
+				</v-slide-group>
+
 				<router-link to="/">
-          <v-btn dark color="#441a61" x-large rounded class="pa-7 mt-10">
-            <v-icon dark left> mdi-arrow-left </v-icon>
-            Voltar
-          </v-btn>
-        </router-link>
+					<v-btn dark color="#441a61" x-large rounded class="pa-7 mt-10">
+						<v-icon dark left> mdi-arrow-left </v-icon>
+						Voltar
+					</v-btn>
+				</router-link>
 			</v-container>
 		</v-main>
 	</v-app>
@@ -60,6 +45,8 @@
 
 <script>
 	import Bar from "@/components/Bar";
+	const axios = require("axios").default;
+
 	export default {
 		components: {
 			Bar,
@@ -67,9 +54,14 @@
 
 		data: () => ({
 			model: null,
-			repositorio: [],
+			repositorio: null,
 		}),
 
+		mounted() {
+			axios
+				.get("https://api.github.com/users/Maicon-Dev/repos")
+				.then((response) => (this.repositorio = response));
+		},
 	};
 </script>
 
@@ -81,7 +73,7 @@
 		height: 100%;
 	}
 
-  h1 {
-    text-align: start;
-  }
+	h1 {
+		text-align: start;
+	}
 </style>
